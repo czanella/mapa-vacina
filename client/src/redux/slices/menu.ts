b37e0eb0dcb38f57d875610b36ad6beb20a7230f
Carlos Zanella <cefzanella@gmail.com>
2021-07-11 17:09:01 -0300

    Setting up Redux in client (WIP)

---
+++ client/src/redux/slices/menu.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from '../store'
+
+// Define a type for the slice state
+interface IMenuState {
+  isOpen: boolean,
+}
+
+// Define the initial state using that type
+const initialState: IMenuState = {
+  isOpen: false,
+};
+
+export const menuSlice = createSlice({
+  name: 'menu',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    openMenu: (state) => {
+      state.isOpen = true;
+    },
+    closeMenu: (state) => {
+      state.isOpen = false;
+    },
+  },
+})
+
+export const { openMenu, closeMenu } = menuSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+export const isMenuOpenSelector = (state: RootState) => state.menu.isOpen;
+
+export default menuSlice.reducer;

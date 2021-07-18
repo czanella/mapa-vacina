3f2cbc54db56baae6ac7f1fb99dfc6dfded1d5e6
Carlos Zanella <cefzanella@gmail.com>
2021-07-18 20:15:29 -0300

    Adds cache to vaccine API

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { vaccineStatus } = require('./vaccineStatus');
+const { vaccineStatus, VACCINE_CACHE_KEY } = require('./vaccineStatus');
+const { useCache } = require('../utils/cache');
 
 const api = express();
 
-api.get('/vaccine-status', vaccineStatus);
+api.get('/vaccine-status', useCache(VACCINE_CACHE_KEY), vaccineStatus);
 
 module.exports = api;
--- server/api/vaccineStatus.js
+++ server/api/vaccineStatus.js
@@ -1,8 +1,11 @@
 const fetch = require('node-fetch');
+const { writeCache } = require('../utils/cache');
+
+const VACCINE_CACHE_KEY = 'vaccines';
 
 const vaccineStatus = async (req, res) => {
   try {
-    const result = await fetch(process.env.VACCINATION_POINTS_ENDPOINT, {
+    const vaccineStatus = await fetch(process.env.VACCINATION_POINTS_ENDPOINT, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
       },
@@ -10,18 +13,21 @@ const vaccineStatus = async (req, res) => {
       method: 'POST',
     });
 
-    const vaccinationPoints = await result.json();
+    const vaccinationPoints = await vaccineStatus.json();
 
-    const data = vaccinationPoints.map((vp) => {
-      const { equipamento, endereco, tipo_posto, status_fila } = vp;
+    const result = {
+      data: vaccinationPoints.map((vp) => {
+        const { equipamento, endereco, tipo_posto, status_fila } = vp;
 
-      return { equipamento, endereco, tipo_posto, status_fila };
-    });
+        return { equipamento, endereco, tipo_posto, status_fila };
+      }),
+    };
 
-    res.json({ data });
+    writeCache(VACCINE_CACHE_KEY, result);
+    res.json(result);
   } catch (error) {
     res.status(500).json({ error: error.toString() });
   }
 };
 
-module.exports = { vaccineStatus };
+module.exports = { vaccineStatus, VACCINE_CACHE_KEY };

934389293a81e07c3612bd55a8392a6cc1316a14
Carlos Zanella <cefzanella@gmail.com>
2021-07-18 19:36:11 -0300

    /api/vaccine-status now fetches real data

--- server/api/vaccineStatus.js
+++ server/api/vaccineStatus.js
@@ -1,5 +1,27 @@
-const vaccineStatus = (req, res) => {
-  res.send('Heyyyyyy!');
+const fetch = require('node-fetch');
+
+const vaccineStatus = async (req, res) => {
+  try {
+    const result = await fetch(process.env.VACCINATION_POINTS_ENDPOINT, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      },
+      body: 'dados=dados',
+      method: 'POST',
+    });
+
+    const vaccinationPoints = await result.json();
+
+    const data = vaccinationPoints.map((vp) => {
+      const { equipamento, endereco, tipo_posto, status_fila } = vp;
+
+      return { equipamento, endereco, tipo_posto, status_fila };
+    });
+
+    res.json({ data });
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
 };
 
 module.exports = { vaccineStatus };

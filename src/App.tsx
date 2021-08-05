be9f1a5dfdef51cfde0c2ed3895629665bb0d2bf
Carlos Zanella <cefzanella@gmail.com>
2021-08-05 07:43:12 -0300

    Adds GMaps JS API

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import { useEffect } from 'react';
+import { Loader as GMapsLoader } from '@googlemaps/js-api-loader';
 import styles from './App.module.scss';
-import geocodes from './data/geocodes.json';
 import { useGetVaccinationStatusesQuery } from './redux/apis';
 
 function App() {
-  useEffect(() => {
-    console.log(geocodes);
-  }, []);
-
+  // Loads the vaccination status query
   const { data, error, isLoading } = useGetVaccinationStatusesQuery('');
+
+  // Loads the Google Maps API
+  useEffect(() => {
+    const loadGmaps = async () => {
+      const loader = new GMapsLoader({
+        apiKey: process.env.REACT_APP_GMAPS_KEY ?? '',
+        version: 'weekly',
+      });
+
+      try {
+        await loader.load();
+      } catch (err) {
+        console.error('Error loading GMaps api', err);
+      }
+    };
+
+    loadGmaps();
+  }, []);
 
   return (
     <div className={styles.app}>

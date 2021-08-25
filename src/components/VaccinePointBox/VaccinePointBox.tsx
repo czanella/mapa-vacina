b88a04509c63f43fd16fe108624f44e91bfb3295
Carlos Zanella <cefzanella@gmail.com>
2021-08-25 08:54:08 -0300

    Colors VaccinePointBox based on queue status

--- src/components/VaccinePointBox/VaccinePointBox.tsx
+++ src/components/VaccinePointBox/VaccinePointBox.tsx
@@ -6,10 +6,22 @@ interface IVaccinePointBoxProps {
   data: IVaccinePoint;
 }
 
+const queueColors: Record<string, string> = {
+  'SEM FILA': '#32CD32',
+  'FILA GRANDE': '#B22222',
+  'FILA MÉDIA': '#FF4500',
+  'FILA PEQUENA': '#FFD700',
+  'AGUARDANDO ABASTECIMENTO 1ª DOSE': '#363636',
+  'NÃO FUNCIONANDO': '#4B0082',
+};
+
 const VaccinePointBox = ({
   data: { equipamento, endereco, tipo_posto, status_fila },
 }: IVaccinePointBoxProps) => (
-  <div className={styles.vaccinePointBox}>
+  <div
+    className={styles.vaccinePointBox}
+    style={{ borderColor: queueColors[status_fila] ?? '#000' }}
+  >
     <p>
       <span className={styles.content}>{equipamento}</span>
     </p>

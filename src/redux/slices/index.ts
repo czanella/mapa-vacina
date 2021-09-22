274eab72ddcc395f70e65fe9093c378b77d2ca40
Carlos Zanella <cefzanella@gmail.com>
2021-09-22 12:54:26 -0300

    Adds cookieConsent slice

--- src/redux/slices/index.ts
+++ src/redux/slices/index.ts
@@ -1,2 +1,3 @@
 export { default as menu } from './menu';
 export { default as selectedVaccinationPoint } from './selectedVaccinationPoint';
+export { default as cookieConsent } from './cookieConsent';
